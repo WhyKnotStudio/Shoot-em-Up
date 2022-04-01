dfa107c6e8e5b03e8edae78f394ad2c1f08e26c0
WhyKnotStudio <101990305+WhyKnotStudio@users.noreply.github.com>
2022-04-02 00:21:22 +0800

    Update PlayerInput.cs
    - removed Input and Shooting function

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -16,37 +16,7 @@ public class PlayerInput : MonoBehaviour
 
     private void Update()
     {
-        // Player Movement
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            this.transform.Translate(Vector2.up * this.m_movementSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            this.transform.Translate(Vector2.down * this.m_movementSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            this.transform.Translate(Vector2.left * this.m_movementSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            this.transform.Translate(Vector2.right * this.m_movementSpeed * Time.deltaTime);
-        }
-
-        // Shoot
-        if (this.m_currentShootInterval > 0f)
-        {
-            this.m_currentShootInterval -= Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.Space) && this.m_currentShootInterval <= 0)
-        {
-            this.Shoot();
-        }
+        // Enter Player Movement and Shooting here
     }
 
     private void Shoot()
